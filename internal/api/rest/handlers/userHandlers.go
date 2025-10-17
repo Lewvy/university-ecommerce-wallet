7da43caa4d3d56d146bacdd153734fc18f3a8705
Lewvy <aditya29december@gmail.com>
2025-10-17 13:09:17 +0530

    Users: defined user routes

--- internal/api/rest/handlers/userHandlers.go
+++ internal/api/rest/handlers/userHandlers.go
@@ -2,18 +2,23 @@ package handlers
 
 import (
 	"ecommerce/internal/api/helpers"
+	"ecommerce/internal/api/rest"
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 type UserHandler struct {
-	app *fiber.App
 }
 
-func (u *UserHandler) UserHandlerRoutes() {
-	u.app.Post("/v1/users/register", u.RegisterUserHandler)
-	u.app.Post("/v1/users/login", u.LoginUserHandler)
+func UserRoutes(rh *rest.RestHandler) {
+	app := rh.App
+
+	//TODO:: create a user service and pass it to handler
+
+	h := UserHandler{}
+	app.Post("/v1/users/register", h.RegisterUserHandler)
+	app.Post("/v1/users/login", h.LoginUserHandler)
 }
 
 func (h *UserHandler) RegisterUserHandler(c *fiber.Ctx) error {

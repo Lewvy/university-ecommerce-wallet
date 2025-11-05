ab9e99686fadb372ad0ec1e8662b1353603bdc2e
Lewvy <aditya29december@gmail.com>
2025-11-05 21:12:28 +0530

    FEAT: integrated mailer

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -9,12 +9,17 @@ import (
 )
 
 type Config struct {
-	Port      string
-	Env       string
-	StartTime time.Time
-	DBString  string
-	DB        *pgxpool.Pool
-	CacheDSN  string
+	Port           string
+	Env            string
+	StartTime      time.Time
+	DBString       string
+	DB             *pgxpool.Pool
+	CacheDSN       string
+	MailerSender   string
+	MailerHost     string
+	MailerPort     string
+	MailerUsername string
+	MailerPassword string
 }
 
 func NewConfig() (cfg Config) {
@@ -22,6 +27,13 @@ func NewConfig() (cfg Config) {
 	cfg.CacheDSN = os.Getenv("CACHE_DSN")
 
 	cfg.DBString = os.Getenv("GOOSE_DBSTRING")
+
+	cfg.MailerHost = os.Getenv("MAILER_HOST")
+	cfg.MailerPort = os.Getenv("MAILER_PORT")
+	cfg.MailerSender = os.Getenv("MAILER_SENDER")
+	cfg.MailerUsername = os.Getenv("MAILER_USERNAME")
+	cfg.MailerPassword = os.Getenv("MAILER_PASSWORD")
+
 	cfg.StartTime = time.Now()
 	flag.StringVar(&cfg.Env, "env", "dev", "set development environment")
 	flag.Parse()

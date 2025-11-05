ec631ddfb2fe046e35d07b825136e161294a4f3d
Lewvy <aditya29december@gmail.com>
2025-11-05 21:19:44 +0530

    FIX: error handling on event of missing mailer port

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -17,25 +19,30 @@ type Config struct {
 	CacheDSN       string
 	MailerSender   string
 	MailerHost     string
-	MailerPort     string
+	MailerPort     int
 	MailerUsername string
 	MailerPassword string
 }
 
-func NewConfig() (cfg Config) {
+func NewConfig() (cfg Config, err error) {
 	cfg.Port = os.Getenv("PORT")
 	cfg.CacheDSN = os.Getenv("CACHE_DSN")
 
 	cfg.DBString = os.Getenv("GOOSE_DBSTRING")
 
 	cfg.MailerHost = os.Getenv("MAILER_HOST")
-	cfg.MailerPort = os.Getenv("MAILER_PORT")
+	mailerPortStr := os.Getenv("MAILER_PORT")
+	cfg.MailerPort, err = strconv.Atoi(mailerPortStr)
+	if err != nil {
+		return Config{}, fmt.Errorf("config error: invalid MAILER_PORT value '%s': %w", mailerPortStr, err)
+	}
 	cfg.MailerSender = os.Getenv("MAILER_SENDER")
 	cfg.MailerUsername = os.Getenv("MAILER_USERNAME")
 	cfg.MailerPassword = os.Getenv("MAILER_PASSWORD")
 
 	cfg.StartTime = time.Now()
 	flag.StringVar(&cfg.Env, "env", "dev", "set development environment")
+
 	flag.Parse()
-	return cfg
+	return cfg, nil
 }
